2c363a6f9b5d9578c1f162d244e27604c0bad0ed
Marisol Pena <penagomezsol@gmail.com>
2022-12-02 19:56:46 +0100

    fix: linter initial errors

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -1,15 +1,14 @@
 import { typography } from '$/styles/themes/typography';
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { $StyledProps } from './types';
-
 
 export const Container = styled.p<$StyledProps>`
   font-weight: ${({ theme }) => theme.weight.regular};
   ${({ $variant }) => typography[$variant]}
   ${({ theme, $color }) =>
-      $color &&
-      css`
+    $color &&
+    css`
       color: ${theme.color[$color]};
     `}
 `;
